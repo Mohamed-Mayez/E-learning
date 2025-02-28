803508a42fa5c0c8240b7f352738e90ca5c43397
Mohamed Mayez <mohamed.n.mayez@gmail.com>
2025-02-28 14:22:43 +0200

    project-setup

---
+++ E-learningPro/E-learningPro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_learningPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
